f9f9956c2748f23c245c542ebe4f3160c9cda6ec
MarcosR1414 <1436181@senati.pe>
2023-09-07 12:43:26 -0500

    login Marcos

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace acceso
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            User = new TextBox();
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(397, 64);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(269, 137);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // User
+            // 
+            User.BackColor = Color.White;
+            User.BorderStyle = BorderStyle.None;
+            User.ForeColor = Color.Silver;
+            User.Location = new Point(411, 269);
+            User.Margin = new Padding(3, 4, 3, 4);
+            User.Multiline = true;
+            User.Name = "User";
+            User.Size = new Size(246, 35);
+            User.TabIndex = 1;
+            User.Text = "Ingrese su usuario";
+            User.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.ForeColor = Color.Silver;
+            textBox1.Location = new Point(411, 322);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(246, 30);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Ingrese tu contraseña";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.BackColor = Color.Transparent;
+            checkBox1.Font = new Font("Gabriola", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            checkBox1.ForeColor = Color.DimGray;
+            checkBox1.Location = new Point(585, 359);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(72, 33);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Mostrar";
+            checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Font = new Font("Calibri", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(467, 388);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Ingresar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(436, 496);
+            button2.Name = "button2";
+            button2.Size = new Size(200, 31);
+            button2.TabIndex = 5;
+            button2.Text = "Registrarme";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 18F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1119, 661);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Controls.Add(User);
+            Controls.Add(pictureBox1);
+            Font = new Font("Calibri", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = Color.Black;
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private TextBox User;
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Registro.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace acceso
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registro));
+            pictureBox1 = new PictureBox();
+            User = new TextBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(397, 64);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(269, 137);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // User
+            // 
+            User.BorderStyle = BorderStyle.None;
+            User.Font = new Font("Calibri", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            User.ForeColor = Color.DarkGray;
+            User.Location = new Point(411, 237);
+            User.Margin = new Padding(3, 4, 3, 4);
+            User.Multiline = true;
+            User.Name = "User";
+            User.Size = new Size(246, 39);
+            User.TabIndex = 2;
+            User.Text = "Nombre del usuario";
+            User.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Calibri", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.DarkGray;
+            textBox1.Location = new Point(329, 300);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(419, 42);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "Correo electronico";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Calibri", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.DarkGray;
+            textBox2.Location = new Point(411, 365);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(246, 40);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "Contraseña";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Gainsboro;
+            button1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(427, 439);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 39);
+            button1.TabIndex = 5;
+            button1.Text = "Registrarme";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Registro
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1119, 661);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(User);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Registro";
+            Text = "Registro";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox User;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
